c6276d93ee8d546be6caaf5d8274092d919ba184
Fausto Zurita <faustodzurita@fb.com>
2019-08-04 21:45:24 -0700

    Added ability to edit items. (Stretch goal #1)

--- app/src/main/java/com/codepath/MainActivity.java
+++ app/src/main/java/com/codepath/MainActivity.java
@@ -1,18 +1,15 @@
 package com.codepath;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import org.apache.commons.io.FileUtils;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -22,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     ListView lvItems;
+
+    public static final int EDIT_REQUEST_CODE = 20;
+    public static final String ITEM_TEXT = "itemText";
+    public static final String ITEM_POSITION = "itemPosition";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +40,31 @@ public class MainActivity extends AppCompatActivity {
         setupListViewListener();
     }
 
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+
+                itemsAdapter.notifyDataSetChanged();
+
+                writeItems();
+
+                return true;
+            }
+        });
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+                i.putExtra(ITEM_TEXT, items.get(position));
+                i.putExtra(ITEM_POSITION, position);
+                startActivityForResult(i, EDIT_REQUEST_CODE);
+            }
+        });
+    }
+
     public void onAddItem(View v) {
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
 
@@ -46,28 +72,11 @@ public class MainActivity extends AppCompatActivity {
 
         itemsAdapter.add(itemText);
 
-        writeItems();
-
         etNewItem.setText("");
 
+        writeItems();
+
         Toast.makeText(getApplicationContext(), "Item added to list", Toast.LENGTH_SHORT).show();
-    }
-
-    private void setupListViewListener() {
-        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long number) {
-                items.remove(position);
-
-                itemsAdapter.notifyDataSetChanged();
-
-                writeItems();
-
-                Log.i("MainActivity", "Removed item " + position);
-
-                return true;
-            }
-        });
     }
 
     private File getDataFile() {
@@ -89,6 +98,24 @@ public class MainActivity extends AppCompatActivity {
             FileUtils.writeLines(getDataFile(), items);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == EDIT_REQUEST_CODE) {
+            String updatedItem = data.getExtras().getString(ITEM_TEXT);
+
+            int position = data.getExtras().getInt(ITEM_POSITION, 0);
+
+            items.set(position, updatedItem);
+
+            itemsAdapter.notifyDataSetChanged();
+
+            writeItems();
+
+            Toast.makeText(this, "Item updated", Toast.LENGTH_SHORT).show();
         }
     }
 }
